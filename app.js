4b7036248454481f7b805d0d018dd3d0b61a4088
Bennett Warner <bennettdwarner@gmail.com>
2021-03-03 12:43:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,60 +15,63 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 //Set Download Directory and create it if it does not exist
 var downloadDir = process.env.DOWNLOAD_DIR;
 
-if (!fs.existsSync(downloadDir)) {
-  fs.mkdirSync(downloadDir);
-}
+// Broken by tiktok changes
 
 // Download video and return to user
-function getVid(tiktok_url, ctx) {
-  siteRE= /\"downloadAddr\":\"https?:\/\/.*?\"/g
-  axios
-    .get(tiktok_url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81",
-      },
-    })
-    .then((response) => {
-      console.log(response.headers)
-      console.log(response.data.match(siteRE)[0].substring(16, response.data.match(siteRE)[0].length - 1).replace(/\\u0026/g, "&"))
-      cookies = response.headers['set-cookie'].toString().replace(/\n|\r/g, "").match(/tt_webid_?v?2?=\d{19};/g).toString().replace(/,/g, ' ')
-      console.log(cookies)
-      let video;
-      try {
-        video = response.data.match(siteRE)[0].substring(16, response.data.match(siteRE)[0].length - 1).replace(/\\u0026/g, "&");}
-      catch(error){
-        console.log(error)
-        ctx.reply("❌");
-        return
-      }
-      video_key = tiktok_url.substring(22, tiktok_url.length - 1);
-      console.log(video_key)
-      if (video_key.length > 9) {
-        video_key = Date.now();
-      }
-      axios({
-        method: "get",
-        url: video,
-        responseType: "stream",
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81",
-            Referer: "https://www.tiktok.com/",
-            Cookie: cookies,
-        },
-      }).then(function (response) {
-        response.data.pipe(
-          fs.createWriteStream(downloadDir + "/" + video_key + ".mp4")
-        );
-        ctx.reply("👍");
-      });
-    })
-    .catch((error) => console.log(error.response));
-}
+// function getVid(tiktok_url, ctx) {
+//   siteRE= /\"downloadAddr\":\"https?:\/\/.*?\"/g
+//   axios
+//     .get(tiktok_url, {
+//       headers: {
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81",
+//       },
+//     })
+//     .then((response) => {
+//       console.log(response.headers)
+//       console.log(response.data.match(siteRE)[0].substring(16, response.data.match(siteRE)[0].length - 1).replace(/\\u0026/g, "&"))
+//       cookies = response.headers['set-cookie'].toString().replace(/\n|\r/g, "").match(/tt_webid_?v?2?=\d{19};/g).toString().replace(/,/g, ' ')
+//       console.log(cookies)
+//       let video;
+//       try {
+//         video = response.data.match(siteRE)[0].substring(16, response.data.match(siteRE)[0].length - 1).replace(/\\u0026/g, "&");}
+//       catch(error){
+//         console.log(error)
+//         ctx.reply("❌");
+//         return
+//       }
+//       video_key = tiktok_url.substring(22, tiktok_url.length - 1);
+//       console.log(video_key)
+//       if (video_key.length > 9) {
+//         video_key = Date.now();
+//       }
+//       axios({
+//         method: "get",
+//         url: video,
+//         responseType: "stream",
+//         headers: {
+//           "User-Agent":
+//             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81",
+//             Referer: "https://www.tiktok.com/",
+//             Cookie: cookies,
+//         },
+//       }).then(function (response) {
+//         response.data.pipe(
+//           fs.createWriteStream(downloadDir + "/" + video_key + ".mp4")
+//         );
+//         ctx.reply("👍");
+//       });
+//     })
+//     .catch((error) => console.log(error.response));
+// }
 
 function getVidYTDL(tiktok_url, ctx) {
-  cmd = "youtube-dl --output '~/userdir/Nextcloud/TikTok/Downloads/%(id)s.mp4' " + tiktok_url;
+      video_key = tiktok_url.substring(22, tiktok_url.length - 1);
+      console.log(video_key)
+      cmd = "youtube-dl --output '" + downloadDir + video_key + ".mp4' " + tiktok_url;
+      if (video_key.length > 9) {
+        cmd = "youtube-dl --output '" + downloadDir + "%(id)s.mp4' " + tiktok_url;
+      }
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
